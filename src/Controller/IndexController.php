66c5a5f6f8852984ef6db3b6960eb4ec94cb5742
janest <julien.anest@evogue.fr>
2020-06-11 16:51:30 +0200

    Page article

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,7 +11,7 @@ class IndexController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index()
+    public function index(ArticleRepository $repository)
     {
         /**
          * Faire une page article dans un contrôleur ArticleController
@@ -22,7 +23,19 @@ class IndexController extends AbstractController
          * Dans la page d'accueil des catégories, lister les 3 derniers articles
          * de la catégorie
          */
+        $articles = $repository->findBy(
+            [],
+            [
+                'publicationDate' => 'DESC'
+            ],
+            3
+        );
 
-        return $this->render('index/index.html.twig');
+        return $this->render(
+            'index/index.html.twig',
+            [
+                'articles' => $articles
+            ]
+        );
     }
 }
